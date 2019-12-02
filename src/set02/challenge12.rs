c4d5221d7fec1b185b1a28c01cae9390631dcc73
eanyanwu <ezeanyinabia.anyanwu@gmail.com>
2019-12-01 22:36:01 -0500

    finish up a very rough, but working draft of the challenge12

--- src/set02/challenge12.rs
+++ src/set02/challenge12.rs
@@ -29,6 +29,8 @@ pub mod test {
     use crate::set02::challenge11;
     use crate::set02::challenge12;
 
+    use std::collections::HashMap;
+
     pub fn byte_at_a_time_ecb_decryption() {
         let random_key = b"0123456789abcdef";
         
@@ -66,6 +68,65 @@ pub mod test {
         let mode = challenge11::detect_cipher_mode(&cipher_text);
 
         assert_eq!(aes128::CipherMode::ECB, mode);
+
+        // At this point, we have confirmed the oracle is indeed using AES ECB
+        // encryption, and we know the block size.
+        // Now for the magic trick..
+
+        let mut secret_message = Vec::new();
+        let shifting_buffer_len = 128;
+
+        for x in 1..=shifting_buffer_len {
+            let mut brute_force_table = HashMap::new();
+            let shifted_input_len = shifting_buffer_len - x;
+            let mut shifted_input = Vec::new();
+            
+            for _ in 0..shifted_input_len {
+                shifted_input.push(b'A');
+            }
+            
+            // Prepare the brute-force table
+            for i in 0..=255 {
+                let mut curr = shifted_input.clone();
+
+                // Add the bytes of the secret message we know of
+                curr.append(&mut secret_message.clone());
+
+                // For my own sanity
+                assert_eq!(curr.len(), shifting_buffer_len - 1);
+
+                curr.push(i);
+                
+                let mut oracle_result = challenge12::cipher_text_oracle(
+                    random_key,
+                    &curr
+                );
+
+                oracle_result.split_off(shifting_buffer_len);
+
+                brute_force_table.insert(
+                    oracle_result,
+                    curr
+                );
+            }
+
+            // Ask the oracle for the encryption of our shifted input
+            let mut oracle_result = challenge12::cipher_text_oracle(
+                random_key,
+                &shifted_input
+            );
+            oracle_result.split_off(shifting_buffer_len);
+
+            let mut plain_text = brute_force_table.remove(&oracle_result)
+                                                    .unwrap();
+
+            secret_message.push(plain_text.pop().unwrap());
+        }
+        
+        assert_eq!(
+            "Rollin\' in my 5.0\nWith my rag-top down so my hair can blow\nThe girlies on standby waving just to say hi\nDid you stop? No, I just",
+            std::str::from_utf8(&secret_message).unwrap()
+        );
     }
 
     #[test]
